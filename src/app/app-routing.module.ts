a7a835b6441fd8d5b07805bc9ce10855cd9e9021
inspekturjr <inspektur20@gmail.com>
2020-11-06 17:02:55 +0700

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { Component, NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LocationComponent } from './location/location.component';
+
+const routes: Routes = [
+  {
+    path:'location',
+    component:LocationComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LocationComponent } from './location/location.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LocationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
